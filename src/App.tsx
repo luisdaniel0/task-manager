fa0f48f307010945da742ca024ee6b7d66f4e16f
Luis Sanchez <luisanchezdh@gmail.com>
2026-04-30 13:00:29 -0400

    added task state and input fields/form for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
 import { useState } from "react";
-import type { Task } from "./types";
+import type { Task, Priority } from "./types";
+
+type TaskState = Partial<Task>;
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [task, setTask] = useState<TaskState>({
+    name: "",
+    details: "",
+    priority: undefined,
+    date: "",
+  });
+
+  /*
+  {
+      id: string;
+      name: string;
+      details?: string;
+      status: Status;
+      priority: Priority;
+      date: string;
+
+      status is added once task is added, defaults to "Not Started"
+      date is added on creation time when task is added
+  }
+
+  //Q's
+    do all forms require a state? i had issues with identifying what kind of states i 
+  needed and got confused because i had a state for a list of tasks and also a state for 
+  individual tasks and it confused me. 
+
+
+  */
+
+  function addTask(task) {}
   return (
     <>
-      <h1>hello</h1>
+      <h2>Task Manager</h2>
+      <form>
+        <label>
+          Title: <input type="text" />
+        </label>
+        <label>
+          Details: <textarea></textarea>
+        </label>
+        <label>
+          Priority:
+          <select name="priority" id="prio">
+            <option value="">--Please choose an option--</option>
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+          </select>
+        </label>
+        <button>Add</button>
+      </form>
     </>
   );
 }
